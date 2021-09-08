a0cd8c48ecd2a409ed2af226540c4818e1472d0d
Asus <v_budaev78@mail.ru>
2021-09-08 13:27:18 +0400

    1. Динамический список на массиве. [#158 #197614]

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -6,39 +6,37 @@ public class SimpleArrayList<T> implements List<T> {
 
     private T[] container;
 
-    private int position;
-
     private int size;
 
     private int modCount;
-
-    private int expectedModCount;
 
     public SimpleArrayList(int capacity) {
         this.container = (T[]) new Object[capacity];
     }
 
-    @Override
-    public void add(T value) {
-        this.container[position++] = value;
-        modCount++;
-        size++;
-        if (position == container.length) {
+    public void expansion() {
+        if (size == container.length) {
             container = Arrays.copyOf(container, container.length * 2);
         }
     }
 
     @Override
+    public void add(T value) {
+        this.container[size++] = value;
+        modCount++;
+    }
+
+    @Override
     public T set(int index, T newValue) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         container[index] = newValue;
         modCount++;
-        return newValue;
+        return container[index];
     }
 
     @Override
     public T remove(int index) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         System.arraycopy(container, index + 1, container, index, container.length - index - 1);
         container[container.length - 1] = null;
         modCount++;
@@ -48,7 +46,7 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         return container[index];
     }
 
@@ -59,24 +57,36 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public Iterator<T> iterator() {
-        return new Iterator<T>() {
-
-            @Override
-            public boolean hasNext() {
-                return position < size;
-            }
-
-            @Override
-            public T next() {
-                expectedModCount++;
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                return container[position++];
-            }
-        };
+        return new SimpleIterator<>(container, size);
     }
+
+    private class SimpleIterator<T> implements Iterator<T> {
+
+        private int index;
+
+        private int expectedModCount = 0;
+
+        private T[] data;
+
+        public SimpleIterator(T[] data, int size) {
+            this.expectedModCount = size;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            return index < size;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return data[index++];
+        }
+    }
+
 }
